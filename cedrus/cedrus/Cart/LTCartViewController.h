cdbc2229b8fb0db5ed23f59fb270b810cc75fda1
赵旭 <zhaoxu@ilingtong.com>
2016-07-27 00:16:00 +0800

    test

---
+++ cedrus/cedrus/Cart/LTCartViewController.h
@@ -0,0 +1,13 @@
+//
+//  LTCartViewController.h
+//  cedrus
+//
+//  Created by X Z on 16/7/27.
+//  Copyright © 2016年 LT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LTCartViewController : UIViewController
+
+@end
---
+++ cedrus/cedrus/Cart/LTCartViewController.m
@@ -0,0 +1,37 @@
+//
+//  LTCartViewController.m
+//  cedrus
+//
+//  Created by X Z on 16/7/27.
+//  Copyright © 2016年 LT. All rights reserved.
+//
+
+#import "LTCartViewController.h"
+
+@interface LTCartViewController ()
+
+@end
+
+@implementation LTCartViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
